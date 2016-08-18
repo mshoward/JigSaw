6c8b0dc22ef08fc22d9610f93e6a3386b6a91976
mshoward <mshoward@mail.lipscomb.edu>
2016-08-18 14:26:42 -0500

    just writing away

---
+++ JigSaw/JigSawMain.java
@@ -0,0 +1,80 @@
+package JigSaw;
+
+/**
+ * Created by mason on 8/18/16.
+ */
+public class JigSawMain implements iJigSaw, Runnable{
+    
+    private JigSawConf conf;
+    
+    public JigSawMain(){
+        conf = new JigSawConf();
+        
+    }
+    
+    
+    @Override
+    public void Connect(iJigSaw piece) {
+        
+    }
+    
+    @Override
+    public void Connect(Iterable<iJigSaw> iterablePieces) {
+        
+    }
+    
+    @Override
+    public void ConnectOut(iJigSaw piece) {
+        
+    }
+    
+    @Override
+    public void ConnectOut(Iterable<iJigSaw> iterablePieces) {
+        
+    }
+    
+    @Override
+    public void ConnectIn(iJigSaw piece) {
+        
+    }
+    
+    @Override
+    public void ConnectIn(Iterable<iJigSaw> iterablePieces) {
+        
+    }
+    
+    @Override
+    public void StartTalking() {
+        
+    }
+    
+    @Override
+    public void StopTalking() {
+        
+    }
+    
+    @Override
+    public void StartListening() {
+        
+    }
+    
+    @Override
+    public void StopListening() {
+        
+    }
+    
+    @Override
+    public void StartThinking() {
+        
+    }
+    
+    @Override
+    public void StopThinking() {
+        
+    }
+    
+    @Override
+    public void run() {
+        
+    }
+}
